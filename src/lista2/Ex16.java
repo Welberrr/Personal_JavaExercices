c56d71d02c73537d638a7c8134ff73266f92be29
Welberrr <welberhenrique.costa@gmail.com>
2024-02-13 23:31:02 -0300

    Feat: Finishing list 2

---
+++ src/lista2/Ex16.java
@@ -0,0 +1,16 @@
+package lista2;
+
+public class Ex16 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Write a Java program to print a face.");
+		System.out.println();
+	
+		System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+	}
+}
